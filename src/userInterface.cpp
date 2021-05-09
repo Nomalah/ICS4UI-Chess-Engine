e26e3ac4f3d8fd69890d9c8c4ba63b65990bcbbe
Nomalah <36043275+Nomalah@users.noreply.github.com>
2021-05-09 04:35:46 -0400

    Reworked position::ascii and fixed a bug with postition::fromFen
    Added game::finished, although unused.
    Changed castling flags and turn flags, added inserting at index.

--- src/userInterface.cpp
+++ src/userInterface.cpp
@@ -8,35 +8,17 @@
 
 std::string chess::position::ascii() {
 	std::string result = " +--------+\n";
-	result.reserve(132); // Size of final string
+	result.reserve(132);  // Size of final string
 
-	u64 targetSquare = chess::util::iter;	
+	chess::u64 targetSquare = chess::util::iter;
 	for (std::size_t y = 8; y > 0; y--) {
 		result += (char)(y + 48);
 		result += "|";
 		for (std::size_t x = 8; x > 0; x--) {
-			if ((this->whiteBishops | this->blackBishops) & targetSquare) {
-				result += 'b';
-			} else if ((this->whitePawns | this->blackPawns) & targetSquare) {
-				result += 'p';
-			} else if ((this->whiteKnights | this->blackKnights) & targetSquare) {
-				result += 'n';
-			} else if ((this->whiteRooks | this->blackRooks) & targetSquare) {
-				result += 'r';
-			} else if ((this->whiteQueens | this->blackQueens) & targetSquare) {
-				result += 'q';
-			} else if ((this->whiteKings | this->blackKings) & targetSquare) {
-				result += 'k';
-			} else {
-				result += '*';
-			}
-
-			if (this->whitePieces & targetSquare) {
-				board[y][x] -= 32;
-			}
+			result += chess::util::constants::pieceToChar(this->pieceAtIndex(y * 8 + x - 9));
 			targetSquare >>= 1;
 		}
-		
+
 		result += "|\n";
 	}
 
@@ -46,8 +28,8 @@ std::string chess::position::ascii() {
 
 chess::position chess::position::fromFen(std::string fen) {
 	if (chess::position::validateFen(fen)) {
-        // Empty position with a valid flag enabled
-		chess::position result = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x80, 0, 0};
+		// Empty position with a valid flag enabled
+		chess::position result = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x04, 0, 0};
 		std::stringstream toTokenize(fen);
 		std::string output;
 		std::array<std::string, 6> tokens;
@@ -56,90 +38,106 @@ chess::position chess::position::fromFen(std::string fen) {
 			*iter++ = output;
 		}
 
-        // Fill board with fen pieces
-		u64 insertAtSquare = chess::util::iter;
+		// Fill board with fen pieces
+		u8 insertIndex = 63;
 		for (char piece : tokens[0]) {
+			u64 insertAtSquare = chess::util::bitboardFromIndex(insertIndex);
 			//std::cout << chess::debugging::convert(insertAtSquare) << std::endl;
 			if (piece > '0' && piece <= '9') {
-				insertAtSquare >>= piece - '0';
-			} else if (piece != '/') {
+                for(std::size_t _ = 0; _ < piece - '0'; _++, insertIndex--){
+					result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::whiteNull);
+				}
+			}
+			else if (piece != '/') {
 				switch (piece) {
 					case 'p':
-						result.blackPieces |= insertAtSquare;
-						result.blackPawns |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::blackPawn);
+						result.bitboards[chess::util::constants::boardModifiers::black] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::blackPawn] |= insertAtSquare;
 						break;
 					case 'P':
-						result.whitePieces |= insertAtSquare;
-						result.whitePawns |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::whitePawn);
+						result.bitboards[chess::util::constants::boardModifiers::white] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::whitePawn] |= insertAtSquare;
 						break;
 					case 'n':
-						result.blackPieces |= insertAtSquare;
-						result.blackKnights |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::blackKnight);
+						result.bitboards[chess::util::constants::boardModifiers::black] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::blackKnight] |= insertAtSquare;
 						break;
 					case 'N':
-						result.whitePieces |= insertAtSquare;
-						result.whiteKnights |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::whiteKnight);
+						result.bitboards[chess::util::constants::boardModifiers::white] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::whiteKnight] |= insertAtSquare;
 						break;
 					case 'b':
-						result.blackPieces |= insertAtSquare;
-						result.blackBishops |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::blackBishop);
+						result.bitboards[chess::util::constants::boardModifiers::black] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::blackBishop] |= insertAtSquare;
 						break;
 					case 'B':
-						result.whitePieces |= insertAtSquare;
-						result.whiteBishops |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::whiteBishop);
+						result.bitboards[chess::util::constants::boardModifiers::white] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::whiteBishop] |= insertAtSquare;
 						break;
 					case 'r':
-						result.blackPieces |= insertAtSquare;
-						result.blackRooks |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::blackRook);
+						result.bitboards[chess::util::constants::boardModifiers::black] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::blackRook] |= insertAtSquare;
 						break;
 					case 'R':
-						result.whitePieces |= insertAtSquare;
-						result.whiteRooks |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::whiteRook);
+						result.bitboards[chess::util::constants::boardModifiers::white] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::whiteRook] |= insertAtSquare;
 						break;
 					case 'q':
-						result.blackPieces |= insertAtSquare;
-						result.blackQueens |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::blackQueen);
+						result.bitboards[chess::util::constants::boardModifiers::black] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::blackQueen] |= insertAtSquare;
 						break;
 					case 'Q':
-						result.whitePieces |= insertAtSquare;
-						result.whiteQueens |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::whiteQueen);
+						result.bitboards[chess::util::constants::boardModifiers::white] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::whiteQueen] |= insertAtSquare;
 						break;
 					case 'k':
-						result.blackPieces |= insertAtSquare;
-						result.blackKings |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::blackKing);
+						result.bitboards[chess::util::constants::boardModifiers::black] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::blackKing] |= insertAtSquare;
 						break;
 					case 'K':
-						result.whitePieces |= insertAtSquare;
-						result.whiteKings |= insertAtSquare;
+						result.insertPieceAtIndex(insertIndex, chess::util::constants::boardModifiers::whiteKing);
+						result.bitboards[chess::util::constants::boardModifiers::white] |= insertAtSquare;
+						result.bitboards[chess::util::constants::boardModifiers::whiteKing] |= insertAtSquare;
 						break;
 				}
-				insertAtSquare >>= 1;
+				insertIndex -= 1;
 			}
 		}
-        
-        // Set turn flag
-		result.flags |= (tokens[1] == "w") << 6;
 
-        // Set castling flags
+		// Set turn flag
+		result.flags |= tokens[1] == "w" ? 0x08 : 0x00;
+
+		// Set castling flags
 		for (char flag : tokens[2]) {
 			switch (flag) {
 				case 'K':
-					result.flags |= 0x08;
-                    break;
-                case 'Q':
-					result.flags |= 0x04;
+					result.flags |= 0x80;
 					break;
-                case 'k':
-					result.flags |= 0x02;
+				case 'Q':
+					result.flags |= 0x40;
+					break;
+				case 'k':
+					result.flags |= 0x20;
 					break;
 				case 'q':
-					result.flags |= 0x01;
+					result.flags |= 0x10;
 					break;
 			}
 		}
 
-        // Set en passant target square
-		result.enPassantTargetSquare = chess::util::algebraicToSquare(tokens[3]);
+		// Set en passant target square
+		result.enPassantTargetSquare = chess::util::bitboardFromIndex(chess::util::algebraicToSquare(tokens[3]));
 
 		// Set halfmove clock
 		result.halfMoveClock = std::stoul(tokens[4]);
@@ -148,15 +146,17 @@ chess::position chess::position::fromFen(std::string fen) {
 		// (Unused and not implimented)
 		return result;
 	} else {
-        // Empty position with valid flag not set
+		// Empty position with valid flag not set
 		return {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	}
 }
 
+bool chess::game::finished() {
+	return false;
+}
 
 chess::game chess::defaultGame() {
 	std::vector<position> gameHistory;
-	gameHistory.push_back(position::fromFen(
-		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
+	gameHistory.push_back(position::fromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
 	return {gameHistory};
 }
