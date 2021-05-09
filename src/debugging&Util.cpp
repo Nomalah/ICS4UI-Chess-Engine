c0de6dd5fe0dae2c23e460bd381c083dc16cd297
Nomalah <36043275+Nomalah@users.noreply.github.com>
2021-05-09 04:09:49 -0400

    Added utility functions, inlined one, removed one
    Moved algebraicToSquare for constexpr inlining
    Added squareToAlgebraic for conversion between an index and a square.
    Added ctz64 & clz64
    Added pieceToChar for position::ascii
    Added colorOf and nullOf for move generation and move making

--- src/debugging&Util.cpp
+++ src/debugging&Util.cpp
@@ -15,25 +15,42 @@ std::string chess::debugging::convert(u64 bitBoard) {
 	return result;
 }
 
-chess::u8 chess::util::algebraicToSquare(std::string algebraic) {
-	if (algebraic.length() == 2) {
-		u8 total = 0;
-		if (algebraic[0] >= 'a' && algebraic[0] <= 'h') {
-			total += 7 - algebraic[0] + 'a';
-			if (algebraic[1] >= '1' && algebraic[1] <= '8') {
-				total += (algebraic[1] - '1') * 8;
-				return 1ULL << total;
-			}
-		}
-	}
-	return 0x0ULL;
+std::string chess::util::squareToAlgebraic(chess::u8 square) {
+	std::string file[] = {"h", "g", "f", "e", "d", "c", "b", "a"};
+	std::string rank[] = {"1", "2", "3", "4", "5", "6", "7", "8"};
+	return file[square % 8] + rank[square / 8];
 }
 
-bool chess::position::pieceEqual(chess::position a, chess::position b) {
-	return std::memcmp(&a, &b, sizeof(chess::u64) * 14);
+chess::u8 chess::util::ctz64(u64 bitboard) {
+#if defined(__clang__) || defined(__GNUC__)
+	return __builtin_ctzll(bitboard);
+#else
+	static_assert(-1);
+#endif
+}
+
+chess::u8 chess::util::clz64(u64 bitboard) {
+#if defined(__clang__) || defined(__GNUC__)
+	return __builtin_clzll(bitboard);
+#else
+	static_assert(-1);
+#endif
 }
 
 // Ignoring for the time being, as it is not necessary to either the AI or engine
 bool chess::position::validateFen(std::string fen) {
 	return true;
+}
+
+char chess::util::constants::pieceToChar(chess::util::constants::boardModifiers piece) {
+	constexpr char conversionList[] = {'*', 'p', 'n', 'b', 'r', 'q', 'k', '*', '*', 'P', 'N', 'B', 'R', 'Q', 'K', '*'};
+	return conversionList[piece];
+}
+
+chess::util::constants::boardModifiers chess::util::colorOf(chess::util::constants::boardModifiers piece){
+	return static_cast<chess::util::constants::boardModifiers>(piece & 0x08);
+}
+
+chess::util::constants::boardModifiers chess::util::nullOf(chess::util::constants::boardModifiers piece){
+	return static_cast<chess::util::constants::boardModifiers>(piece | 0x07);
 }
