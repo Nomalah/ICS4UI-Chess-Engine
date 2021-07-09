3005da3d4f0aa1705e292d00691e9d1435fe624b
Nomalah <nomalahvids@gmail.com>
2021-07-09 18:30:09 -0400

    Direct mapping instead of forcing std::string addition

--- src/debugging&Util.cpp
+++ src/debugging&Util.cpp
@@ -16,9 +16,19 @@ std::string chess::debugging::bitboardToString(u64 bitBoard) {
 }
 
 std::string chess::util::squareToAlgebraic(const chess::squareAnnotations square) {
-	std::string file[] = { "h", "g", "f", "e", "d", "c", "b", "a" };
-	std::string rank[] = { "1", "2", "3", "4", "5", "6", "7", "8" };
-	return file[square % 8] + rank[square / 8];
+	// clang-format off
+	static constexpr const char* mapping[] {
+		"h1", "g1", "f1", "e1", "d1", "c1", "b1", "a1",
+		"h2", "g2", "f2", "e2", "d2", "c2", "b2", "a2",
+        "h3", "g3", "f3", "e3", "d3", "c3", "b3", "a3",
+        "h4", "g4", "f4", "e4", "d4", "c4", "b4", "a4",
+        "h5", "g5", "f5", "e5", "d5", "c5", "b5", "a5",
+        "h6", "g6", "f6", "e6", "d6", "c6", "b6", "a6",
+        "h7", "g7", "f7", "e7", "d7", "c7", "b7", "a7",
+        "h8", "g8", "f8", "e8", "d8", "c8", "b8", "a8",
+	};
+	// clang-format on
+	return mapping[square];
 }
 
 // Ignoring for the time being, as it is not necessary to either the AI or engine
